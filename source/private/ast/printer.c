a1420b10163578c8b32b249af57442f14659a9ef
Phytolizer <kyle.coffey@tamu.edu>
2021-08-07 23:24:52 -0500

    add test for ast printer

--- source/private/ast/printer.c
+++ source/private/ast/printer.c
@@ -6,8 +6,6 @@
 
 #define TOKEN_PREFIX_LEN 8
 
-static char* dump_token(const struct token* token);
-
 void print_ast(struct expr* expr)
 {
   struct ast_printer printer;
@@ -18,7 +16,7 @@ char* ast_printer_visit_binary_expr(struct ast_printer* printer,
                                     struct binary_expr* expr)
 {
   char* left = expr_accept_ast_printer(expr->left, printer);
-  char* op = dump_token(&expr->op);
+  char* op = expr->op.lexeme;
   char* right = expr_accept_ast_printer(expr->right, printer);
 
   return alloc_printf("(%s %s %s)", op, left, right);
@@ -44,22 +42,9 @@ char* ast_printer_visit_literal_expr(struct ast_printer* printer,
 char* ast_printer_visit_unary_expr(struct ast_printer* printer,
                                    struct unary_expr* expr)
 {
-  char* op = dump_token(&expr->op);
+  const char* op = expr->op.lexeme;
   char* right = expr_accept_ast_printer(expr->right, printer);
   return alloc_printf("(%s%s)", op, right);
 }
 
-static char* dump_token(const struct token* token)
-{
-  size_t toklen = token_snprint(NULL, 0, token);
-  // including null terminator in len because of sizeof()
-  size_t len = toklen + sizeof("Token {  }");
-  char* str = GC_MALLOC(len);
-  char* p = str;
-  p += sprintf(p, "Token { ");
-  p += token_snprint(p, toklen, token);
-  sprintf(p, " }");
-  return str;
-}
-
-EXPR_DEFINE_ACCEPT_FOR(char*, ast_printer)
+EXPR_DEFINE_ACCEPT_FOR(char*, ast_printer);
--- test/source/gc-c-jlox_test.c
+++ test/source/gc-c-jlox_test.c
@@ -1,12 +1,47 @@
 #include <lib.h>
+#include <private/ast/expr.h>
+#include <private/ast/printer.h>
 #include <string.h>
+
+static int test_ast_printer(void);
 
 int main(int argc, const char* argv[])
 {
   (void)argc;
   (void)argv;
 
-  library lib = create_library();
+  int ret;
 
-  return strcmp(lib.name, "gc-c-jlox") == 0 ? 0 : 1;
+  if ((ret = test_ast_printer())) {
+    return ret;
+  }
+  return 0;
+}
+
+static int test_ast_printer(void)
+{
+  struct ast_printer printer;
+  struct expr* expr = (struct expr*)expr_new_binary(
+      (struct expr*)expr_new_literal(OBJECT_NUMBER(1)),
+      (struct token) {
+          .type = TOKEN_PLUS,
+          .lexeme = "+",
+          .literal = OBJECT_NULL(),
+          .line = 1,
+      },
+      (struct expr*)expr_new_binary(
+          (struct expr*)expr_new_literal(OBJECT_NUMBER(2)),
+          (struct token) {
+              .type = TOKEN_STAR,
+              .lexeme = "*",
+              .literal = OBJECT_NULL(),
+              .line = 1,
+          },
+          (struct expr*)expr_new_literal(OBJECT_NUMBER(3))));
+  const char* printed = expr_accept_ast_printer(expr, &printer);
+  int ret = strcmp(printed, "(+ 1 (* 2 3))");
+  if (ret) {
+    printf("%s\n", printed);
+  }
+  return ret;
 }
